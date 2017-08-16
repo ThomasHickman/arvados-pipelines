7391dc0bcbb4815ed8fd535229e7ef40333639e1
ThomasHickman <Thomas.Hickman42@gmail.com>
2017-08-16 17:11:52 +0100

    cwl_files -> gatk_cmdline_tools

--- cwl/get_haplotype_caller.sh
+++ cwl/get_haplotype_caller.sh
@@ -1,5 +1,5 @@
 tmp_folder=`mktemp -d`
-curl -L $(python get_latest_release.py) > $tmp_folder/cwl_files.zip
-unzip $tmp_folder/cwl_files.zip -d $tmp_folder
-cp $tmp_folder/cwl_files/cwl_files_3.5/cwl/HaplotypeCaller.cwl ./HaplotypeCaller.cwl
+curl -L $(python get_latest_release.py) > $tmp_folder/gatk_cmdline_tools.zip
+unzip $tmp_folder/gatk_cmdline_tools.zip -d $tmp_folder
+cp $tmp_folder/gatk_cmdline_tools/3.5/cwl/HaplotypeCaller.cwl ./HaplotypeCaller.cwl
 rm -r $tmp_folder
